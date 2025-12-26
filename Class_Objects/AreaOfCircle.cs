bef7ceec247233bbd0cd1d526f8c66a2023df672
abhay9876 <singhabhayrna987@gmail.com>
2025-12-26 14:48:47 +0530

    [Abhay Singh]Add . Area Of Circle problem solved.

---
+++ Class_Objects/AreaOfCircle.cs
@@ -0,0 +1,29 @@
+﻿/*Program to Compute Area of a Circle
+Problem Statement: Write a program to create a Circle class with an attribute radius. Add*/
+
+
+using System;
+
+public class AreaOfCircle
+{
+	//Fields (Attributes)
+	double radius;
+
+	//Constructor
+	public AreaOfCircle(double radius)
+	{
+		this.radius = radius;
+	}
+
+	//Method to Calculate and dispaly output
+
+	public void CalculateArea()
+	{
+		double result = Math.PI * Math.Pow(radius, 2);
+		Console.WriteLine("The radius  is : " + radius);
+
+		Console.WriteLine("The Area of Circle is : " + result);
+	}
+
+
+}
