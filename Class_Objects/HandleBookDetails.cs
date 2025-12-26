4d3de575321515a71766ae884a39c0440d61a2cc
abhay9876 <singhabhayrna987@gmail.com>
2025-12-26 14:47:20 +0530

    [Abhay Singh]Add . Handle Book Details problem solved.

---
+++ Class_Objects/HandleBookDetails.cs
@@ -0,0 +1,34 @@
+﻿/*Program to Handle Book Details
+Problem Statement: Write a program to create a Book class with attributes title, author, and
+price. Add a method to display the book details.*/
+
+
+
+using System;
+
+public class HandleBookDetails
+{
+    //Fields (Attributes)
+    string title;
+    string author;
+    int price;
+
+    //Constructor
+    public HandleBookDetails(string title, string author, int price)
+    {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+
+    //Display Book Details
+    public void BookDetails()
+    {
+        Console.WriteLine("Book Details : ");
+        Console.WriteLine(" Title : " + title);
+        Console.WriteLine(" Author  : " + author);
+        Console.WriteLine(" Price : " + price);
+
+    }
+}
