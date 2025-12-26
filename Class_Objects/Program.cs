96123b6a89405dc5f8601821a3f682932dec7a76
abhay9876 <singhabhayrna987@gmail.com>
2025-12-26 14:50:42 +0530

    [Abhay Singh]Add . Class and Objects level 1 Completed.

--- Class_Objects/Program.cs
+++ Class_Objects/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Class_Objects
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeDetails ed = new EmployeeDetails("Abhay", 22, 25000);
+            Console.WriteLine($"Employee Detail :");
+            ed.DisplayDetails();
+
+            EmployeeDetails ed1 = new EmployeeDetails("Jayakant", 80, 25000);
+            Console.WriteLine($"Employee Detail :");
+            ed1.DisplayDetails();
+
+
+            AreaOfCircle ac = new AreaOfCircle(12);
+            ac.CalculateArea();
+            AreaOfCircle ac1 = new AreaOfCircle(13);
+            ac1.CalculateArea();
+
+
+            HandleBookDetails hd = new HandleBookDetails("The IGL", "Zeno", 2000000);
+            hd.BookDetails();
+            HandleBookDetails hd1 = new HandleBookDetails("The Scout", "Monster", 200000);
+            hd1.BookDetails();
+        }
+    }
+}
