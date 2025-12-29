5afeb13437cf605c6070e12d03038f15ed424975
abhay9876 <singhabhayrna987@gmail.com>
2025-12-29 12:10:31 +0530

    [Abhay Singh] Add. Employee Records problem solved

---
+++ Access_Modifier/EmployeeRecords.cs
@@ -0,0 +1,62 @@
+﻿/*Employee Records
+● Develop an Employee class with:
+o employeeID (public)
+o department (protected)
+o salary (private)
+● Implement methods to:
+
+o Modify salary using a public method.
+o Create a subclass Manager to access employeeID and department.*/
+
+
+
+
+
+using System;
+
+public class Employee
+{
+    public int employeeID;
+    protected string department;
+    private double salary;
+
+    // Constructor
+    public Employee(int employeeID, string department, double salary)
+    {
+        this.employeeID = employeeID;
+        this.department = department;
+        this.salary =salary;
+    }
+
+    // Public Method for Access Salary
+    public double GetSalary()
+    {
+        return salary;
+    }
+
+    // Modify the Salary
+    public void SetSalary(double salary)
+    {
+        this.salary = salary;
+    }
+}
+
+// Sub Class
+public class Manager : Employee
+{
+    public Manager(int employeeID, string department, double salary) : base(employeeID, department, salary)
+    {
+        
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Employee ID  :  {employeeID}");
+        Console.WriteLine($"Department  :  {department}");
+        Console.WriteLine($"Salary :  {GetSalary()}");
+        
+
+    }
+}
+
+
