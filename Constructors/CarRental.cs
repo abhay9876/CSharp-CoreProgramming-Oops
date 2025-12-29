2845e372eaa9b9bbda27e6579d10e3fd5f172ada
abhay9876 <singhabhayrna987@gmail.com>
2025-12-29 11:47:39 +0530

    [Abhay Singh] Add . Vehicle Reatal Problem Solved

---
+++ Constructors/CarRental.cs
@@ -0,0 +1,31 @@
+﻿/*Car Rental System
+o Create a CarRental class with attributes customerName, carModel, and
+rentalDays.
+o Add constructors to initialize the rental details and calculate total cost.*/
+
+
+using System;
+
+public class CarRental
+{
+	string customerName;
+	string carModel;
+	int rentalDays;
+
+
+	public CarRental(string customerName, string carModel, int rentalDays)
+	{
+		this.customerName = customerName;
+		this.carModel = carModel;
+		this.rentalDays = rentalDays;
+	}
+
+
+	public void RentalDetails()
+	{
+		Console.WriteLine($"Customer Name  : {customerName}");
+			Console.WriteLine($"Car Model  : {carModel}");
+		Console.WriteLine($"Rental Days  : {rentalDays}");
+		Console.WriteLine($"Total Cost  : {1500 * rentalDays} ");
+	}
+}
