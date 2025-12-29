a1bcf9577d92972aa8ca540e06b9384286b6000d
abhay9876 <singhabhayrna987@gmail.com>
2025-12-29 11:51:08 +0530

    [Abhay Singh] Add . Circle Class Problem Solved

---
+++ Constructors/CircleClass.cs
@@ -0,0 +1,30 @@
+﻿/*Circle Class
+o Write a Circle class with a radius attribute.
+o Use constructor chaining to initialize radius with both default and
+user-provided values.*/
+
+using System;
+
+public class CircleClass
+{
+    //Fields (Attributes)
+    double radius;
+
+    // Default Constructor
+    public CircleClass() : this(2.0)
+    {
+        
+    }
+
+    // Parameterized Constructor
+    public CircleClass(double radius)
+    {
+        this.radius = radius;
+    }
+
+
+    public void Display()
+    {
+        Console.WriteLine($"The radius is : {radius}");
+    }
+}
