2574ff5c7ebfd583e241582cb826d6f997c47d81
abhay9876 <singhabhayrna987@gmail.com>
2025-12-29 11:46:58 +0530

    [Abhay Singh] Add . Book Class Problem Solved

---
+++ Constructors/BookClass.cs
@@ -0,0 +1,37 @@
+﻿/*Book Class
+o Create a Book class with attributes title, author, and price.
+o Provide both default and parameterized constructors.*/
+
+
+using System;
+
+public class BookClass
+{
+	string title;
+	string author;
+	int price;
+
+	public BookClass()
+	{
+		title = "The War";
+		author = "Zeno";
+		price = 250000;
+	}
+
+	public BookClass(string title, string author , int price)
+	{
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+
+
+	public void Display()
+	{
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Author: {author}");
+        Console.WriteLine($"Price: {price}");
+        Console.WriteLine("--------------------");
+    }
+	
+}
