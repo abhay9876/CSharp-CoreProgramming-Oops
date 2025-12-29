f808315721f5a7ca9b3726ad6de2b71d93541a6b
abhay9876 <singhabhayrna987@gmail.com>
2025-12-29 11:51:53 +0530

    [Abhay Singh] Add . Copy Constructors Problem Solved

---
+++ Constructors/CopyConstructors.cs
@@ -0,0 +1,35 @@
+﻿/*Person Class (Copy Constructor)
+o Create a Person class with a copy constructor that clones another person's
+attributes.*/
+
+
+
+using System;
+
+public class Person
+{
+	string name;
+	int age;
+
+
+	// Parameterized Constructor
+    public Person(string name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	// Copy Constructor
+	public Person(Person other)
+	{
+		this.name = other.name;
+		this.age = other.age;
+	}
+
+
+	public void Display()
+	{
+		Console.WriteLine($"Name : {name}");
+        Console.WriteLine($"Age : {age}");
+    }
+}
