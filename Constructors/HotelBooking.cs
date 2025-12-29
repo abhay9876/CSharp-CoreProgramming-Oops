80307a7099ded24ed7f21935332c3d5bc1e13e03
abhay9876 <singhabhayrna987@gmail.com>
2025-12-29 11:52:39 +0530

    [Abhay Singh] Add . Hotel Booking Problem Solved

---
+++ Constructors/HotelBooking.cs
@@ -0,0 +1,44 @@
+﻿/*Hotel Booking System
+o Create a HotelBooking class with attributes guestName, roomType, and
+nights.
+o Use default, parameterized, and copy constructors to initialize bookings.*/
+
+
+using System;
+
+public class HotelBooking
+{
+	string guestName;
+	string roomType;
+	int nights;
+
+	public HotelBooking()
+	{
+		guestName = "Goku";
+		roomType = "AC";
+		nights = 3;
+	}
+
+	public HotelBooking(string guestName , string roomType, int nights)
+	{
+		this.guestName = guestName;
+		this.roomType = roomType;
+		this.nights = nights;
+	}
+
+
+	public HotelBooking(HotelBooking booking)
+	{
+		this.guestName = booking.guestName;
+		this.roomType = booking.roomType;
+		this.nights = booking.nights;
+	}
+
+
+	public void Display()
+	{
+		Console.WriteLine($"Guest Name : {guestName}");
+        Console.WriteLine($"Room Type : {roomType}");
+        Console.WriteLine($"Nights : {nights}");
+    }
+}
