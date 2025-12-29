f9d3f1514852a3ef0ac2831f383b1b00669b76c8
abhay9876 <singhabhayrna987@gmail.com>
2025-12-29 16:58:09 +0530

    [Abhay Singh] Add . keywords and isOperators problem solved.

---
+++ Operator_Keywords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Operator_Keywords
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Bank Management System
+            Bank.Run();
+
+            Library Management System
+            LibraryManagement.Run();
+
+
+            Employee Management
+            EmployeeManagement.Run();
+
+            Shopping Cart System
+             ShoppingCart.Run();
+
+            University Student Management
+            University.Run();
+
+            Vehicle Registration system
+             VehicleRegistration.Run();
+
+            Hospital Management
+            Hospital.Run();
+        }
+    }
+}
