330e80adb7dd7292c80a0cbeac67d853634c946b
abhay9876 <singhabhayrna987@gmail.com>
2026-01-01 22:31:06 +0530

    [Abhay Singh] Add. E-commerce Problem Solved

---
+++ Object_Modelling/ECommerce.cs
@@ -0,0 +1,69 @@
+﻿/*Problem 4: E-commerce Platform with Orders, Customers, and Products
+Description: Design an e-commerce platform with Order, Customer, and Product classes.
+Model relationships where a Customer places an Order, and each Order contains multiple
+Product objects.
+Goal: Show communication and object relationships by designing a system where customers
+communicate through orders, and orders aggregate products.*/
+
+
+
+using System;
+
+class Order
+{
+    public int OrderId { get; }
+    private List<Product> products = new List<Product>();
+
+    public Order(int orderId)
+    {
+        OrderId = orderId;
+    }
+
+    // Aggregation Product comes from outside
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public void ShowOrderDetails()
+    {
+        Console.WriteLine($"Order ID: {OrderId}");
+        double total = 0;
+
+        foreach (var p in products)
+        {
+            Console.WriteLine($" - {p.Name} : ₹{p.Price}");
+            total += p.Price;
+        }
+
+        Console.WriteLine($"Total Amount: ₹{total}");
+    }
+}
+class Product
+{
+    public string Name { get; }
+    public double Price { get; }
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
+
+class Customer
+{
+    public string Name { get; }
+
+    public Customer(string name)
+    {
+        Name = name;
+    }
+
+    // Communication method
+    public void PlaceOrder(Order order)
+    {
+        Console.WriteLine($"Customer {Name} placed Order ID {order.OrderId}");
+        order.ShowOrderDetails();
+    }
+}
